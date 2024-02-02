79172457eaa4555cf20d24a64c7f216703012de1
mteusbarbosa <mateus.bm13@gmail.com>
2024-02-02 09:08:24 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import Button from './components/Button'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
+
+export default App
