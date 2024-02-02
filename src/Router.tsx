b38522e85bd6ab8da915c3aff63c5b8a1dc35aa4
mteusbarbosa <mateus.bm13@gmail.com>
2024-02-02 09:59:14 -0300

    Configuring Route Layoult

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import History from './pages/History'
+import { DefaultLayoult } from './layoults/DefaultLayoult'
 
 export default function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/history" element={<History />}></Route>
+      <Route path="/" element={<DefaultLayoult />}>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/history" element={<History />}></Route>
+      </Route>
     </Routes>
   )
 }
