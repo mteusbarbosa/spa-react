5bded5647fdaed1e77fa23ee06f082f5628c2430
mteusbarbosa <mateus.bm13@gmail.com>
2024-02-21 22:40:34 -0300

    Página: History

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
-import History from './pages/History'
 import { DefaultLayout } from './layoults/DefaultLayout/DefaultLayout'
 import { Home } from './pages/Home'
+import { History } from './pages/History'
 
 export default function Router() {
   return (
