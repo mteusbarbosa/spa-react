0be24db85a82eac83784aea526082dcd127c93c2
mteusbarbosa <mateus.bm13@gmail.com>
2024-03-11 16:25:47 -0300

    Marcando Ciclos como Finalizados

--- src/Contexts/CyclesContext.tsx
+++ src/Contexts/CyclesContext.tsx
@@ -41,9 +41,6 @@ export function CyclesContextProvider({
 }: CyclesContextProviderProps) {
   const [cyclesState, dispatch] = useReducer(
     (state: CycleState, action: any) => {
-      /* console.log(state)
-    console.log(action) */
-
       if (action.type === 'ADD_NEW_CYCLE') {
         return {
           ...state,
@@ -66,7 +63,19 @@ export function CyclesContextProvider({
         }
       }
 
-      /* if (action.type === 'MARK_CURRENT_CYCLE_AS_FINISHED') {} */
+      if (action.type === 'MARK_CURRENT_CYCLE_AS_FINISHED') {
+        return {
+          ...state,
+          cycles: state.cycles.map((cycle) => {
+            if (cycle.id === state.activeCycleId) {
+              return { ...cycle, finishedDate: new Date() }
+            } else {
+              return cycle
+            }
+          }),
+          activeCycleId: null,
+        }
+      }
 
       return state
     },
@@ -108,9 +117,6 @@ export function CyclesContextProvider({
         newCycle,
       },
     })
-
-    // setCycles((state) => [...state, newCycle])
-
     setAmountSecondsPassed(0)
   }
 
@@ -121,15 +127,6 @@ export function CyclesContextProvider({
         activeCycleId,
       },
     })
-    /* setCycles((state) =>
-      state.map((cycle) => {
-        if (cycle.id === activeCycleId) {
-          return { ...cycle, interruptedDate: new Date() }
-        } else {
-          return cycle
-        }
-      }),
-    ) */
   }
 
   return (
